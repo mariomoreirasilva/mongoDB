bfa8efd68f7621561ac6e51736f8aeb3c56363f1
mariomoreirasilva <mario@desban.org.br>
2024-11-07 15:42:57 -0300

    FindById

--- src/main/java/com/devsuperior/mongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/mongo/controllers/UserController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PatchExchange;
 
 import com.devsuperior.mongo.models.dto.UserDTO;
 import com.devsuperior.mongo.services.UserService;
@@ -23,5 +25,13 @@ public class UserController {
 		List<UserDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findaById(@PathVariable String id){
+		UserDTO userDto = service.findById(id);
+		return ResponseEntity.ok().body(userDto);
+	}
+	
+	
 
 }
--- src/main/java/com/devsuperior/mongo/services/UserService.java
+++ src/main/java/com/devsuperior/mongo/services/UserService.java
@@ -1,7 +1,7 @@
 package com.devsuperior.mongo.services;
 
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.mongo.models.dto.UserDTO;
 import com.devsuperior.mongo.models.entities.User;
 import com.devsuperior.mongo.repositories.UserRepository;
+import com.devsuperior.mongo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,6 +22,17 @@ public class UserService {
 		List<User> list = repository.findAll();
 		
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	public UserDTO findById(String id) {
+		
+		Optional<User> obj = repository.findById(id);
+		
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado."));
+		
+		return new UserDTO(entity);
+		
+		
 	}
 
 }
